a8d17e8d212435ce89eed20d33a1d98dea9ae725
samsudinr <samsudiney@gmail.com>
2021-01-06 11:19:59 +0700

    Update import_to_spreadsheet.py

--- ImportDF_toSpreadsheet/import_to_spreadsheet.py
+++ ImportDF_toSpreadsheet/import_to_spreadsheet.py
@@ -1,7 +1,19 @@
 import gspread
+import pandas as pd
 import lib_gspread as lib
 
+cred = lib.cred_emcanalyticsteam
 credentials = lib.authorize_credentials(cred)
 gc = gspread.authorize(credentials)
 id_source = gc.open_by_key("id-speadsheet")
-lib.pandas_to_sheets(results, id_source.worksheet("results"))
+
+# create dataframe
+DF = pd.DataFrame({'Day': [31, 30, 31, 30], 'Month': ['Jan', 'Apr', 'Mar', 'June']})
+
+try:
+    lib.pandas_to_sheets(DF, id_source.worksheet("results"))
+    print ("success import dataframe to spreadsheet")
+except Exception as e:
+    print(e)
+    error = "Error import data to spreadsheet"
+    print (error)
